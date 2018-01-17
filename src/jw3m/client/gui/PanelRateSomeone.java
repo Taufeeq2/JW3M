cba90e1aa1aa7ae1eabdfa9b52c986ebee48b55e
a149936 <a149936@00749JNBFND016L.za.sbicdirectory.com>
2018-01-17 13:35:21 +0200

    d

--- src/jw3m/client/gui/PanelRateSomeone.java
+++ src/jw3m/client/gui/PanelRateSomeone.java
@@ -2,18 +2,23 @@ package jw3m.client.gui;
 
 import javax.swing.JPanel;
 import javax.swing.JLabel;
-import java.awt.Font;
-import javax.swing.JButton;
+import javax.swing.JOptionPane;
 
-public class PanelRateSomeone extends JPanel 
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import java.awt.Color;
+
+public class PanelRateSomeone extends JPanel implements ActionListener
 {
 	private SkillsClient baseFrame;
 	private JLabel lblRateSomeone;
 	private JButton btnSubmit;
-	private JPanel panel;
-	private JPanel panel_1;
 	
 	public PanelRateSomeone(SkillsClient frame) {
+		setBackground(new Color(0, 153, 255));
 		setLayout(null);
 		
 		lblRateSomeone = new JLabel("Rate Someone");
@@ -22,16 +27,22 @@ public class PanelRateSomeone extends JPanel
 		add(lblRateSomeone);
 		
 		btnSubmit = new JButton("Submit");
-		btnSubmit.setBounds(334, 541, 97, 25);
+		btnSubmit.setBounds(334, 484, 97, 25);
 		add(btnSubmit);
+		btnSubmit.addActionListener(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e)
+	{
+		Object source = e.getSource();
 		
-		panel = new JPanel();
-		panel.setBounds(0, 0, 820, 283);
-		add(panel);
+		if(source == btnSubmit)
+		{
+			JOptionPane.showMessageDialog(this, "You have been chosen to rate me!");
+			
+		}
 		
-		panel_1 = new JPanel();
-		panel_1.setBounds(0, 281, 820, 256);
-		add(panel_1);
 	}
 
 }
