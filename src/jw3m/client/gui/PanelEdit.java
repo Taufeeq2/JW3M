38de8041b863b8595b7087ae8efac2d37ef8e759
a149936 <a149936@00749JNBFND016L.za.sbicdirectory.com>
2018-01-17 12:55:44 +0200

    Updated panels for notifications and edit

--- src/jw3m/client/gui/PanelEdit.java
+++ src/jw3m/client/gui/PanelEdit.java
@@ -1,8 +1,22 @@
 package jw3m.client.gui;
 
 import javax.swing.JPanel;
+import javax.swing.JLabel;
 
 public class PanelEdit extends JPanel 
 {
+	
+	private SkillsClient baseFrame;
+	private JLabel lblEditProfile;
+	
+	public PanelEdit(SkillsClient frame) {
+		setLayout(null);
+		
+		lblEditProfile = new JLabel("Edit Profile");
+		lblEditProfile.setBounds(335, 13, 56, 16);
+		add(lblEditProfile);
+	}
+	
+	
 
 }
--- src/jw3m/client/gui/PanelNotifications.java
+++ src/jw3m/client/gui/PanelNotifications.java
@@ -1,8 +1,21 @@
 package jw3m.client.gui;
 
 import javax.swing.JPanel;
+import javax.swing.JLabel;
 
 public class PanelNotifications extends JPanel 
-{ 
-
+{
+	
+	private SkillsClient baseFrame;
+	private JLabel lblNotifications;
+	
+	public PanelNotifications(SkillsClient frame) {
+		setLayout(null);
+		
+		lblNotifications = new JLabel("Notifications");
+		lblNotifications.setBounds(345, 23, 56, 16);
+		add(lblNotifications);
+	} 
+	
+	
 }
