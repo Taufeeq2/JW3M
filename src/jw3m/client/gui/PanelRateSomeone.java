762023eba8f92db23ce5d2ccdd696ea620cedd7d
a149936 <a149936@00749JNBFND016L.za.sbicdirectory.com>
2018-01-17 11:16:25 +0200

    updated rate someone panel

--- src/jw3m/client/gui/PanelRateSomeone.java
+++ src/jw3m/client/gui/PanelRateSomeone.java
@@ -1,8 +1,19 @@
 package jw3m.client.gui;
 
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 public class PanelRateSomeone extends JPanel 
 {
+	private SkillsClient baseFrame;
+	private JLabel lblRateSomeone;
+	
+	public PanelRateSomeone(SkillsClient frame) {
+		
+		lblRateSomeone = new JLabel("Rate Someone");
+		lblRateSomeone.setFont(new Font("Calibri", Font.BOLD | Font.ITALIC, 22));
+		add(lblRateSomeone);
+	}
 
 }
