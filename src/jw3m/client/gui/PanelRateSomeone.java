6f9fae8669534f61a3b12e2b822be8ce43a80653
a149936 <a149936@00749JNBFND016L.za.sbicdirectory.com>
2018-01-17 11:26:11 +0200

    m

--- src/jw3m/client/gui/PanelRateSomeone.java
+++ src/jw3m/client/gui/PanelRateSomeone.java
@@ -3,17 +3,25 @@ package jw3m.client.gui;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import java.awt.Font;
+import javax.swing.JButton;
 
 public class PanelRateSomeone extends JPanel 
 {
 	private SkillsClient baseFrame;
 	private JLabel lblRateSomeone;
+	private JButton btnSubmit;
 	
 	public PanelRateSomeone(SkillsClient frame) {
+		setLayout(null);
 		
 		lblRateSomeone = new JLabel("Rate Someone");
+		lblRateSomeone.setBounds(311, 5, 134, 27);
 		lblRateSomeone.setFont(new Font("Calibri", Font.BOLD | Font.ITALIC, 22));
 		add(lblRateSomeone);
+		
+		btnSubmit = new JButton("Submit");
+		btnSubmit.setBounds(334, 541, 97, 25);
+		add(btnSubmit);
 	}
 
 }
