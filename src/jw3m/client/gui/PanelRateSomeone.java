df73ddc56527a971fc086d9f124fc32ce2860fd6
a149936 <a149936@00749JNBFND016L.za.sbicdirectory.com>
2018-02-02 13:34:24 +0200

    vgf

--- src/jw3m/client/gui/PanelRateSomeone.java
+++ src/jw3m/client/gui/PanelRateSomeone.java
@@ -10,6 +10,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseListener;
 import java.rmi.server.SocketSecurityException;
+import java.util.Collection;
 import java.util.StringTokenizer;
 import java.util.Vector;
 import javax.swing.JButton;
